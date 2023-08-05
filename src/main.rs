dfa335273151efe9e46d038bb6b2d9acfab55dea
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-08-04 23:47:33 -0400

    Added handle_request to get https request data

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,29 @@ use std::{
 
 fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("127.0.0.1:7878")?;
+    let mut http_requests: Vec<String> = Vec::new();
 
     for (connection_id, stream) in listener.incoming().enumerate() {
-        let _stream = stream?;
-        println!("Connection #{}", connection_id + 1);
+        let stream: TcpStream = stream?;
+        println!("Handling Connection #{}", connection_id + 1);
+        let mut http_request: Vec<String> = handle_connection(stream);
+        http_requests.append(&mut http_request);
     }
+
+    println!("{} http requests", http_requests.len());
     
     return Ok(());
+}
+
+fn handle_connection(mut stream: TcpStream) -> Vec<String> {
+    let buf_reader: BufReader<&TcpStream> = BufReader::new(&mut stream);
+
+    let http_request: Vec<String> = buf_reader
+        .lines()
+        .map(|result| result.unwrap()) // explore more robust error handling
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("{:?}", http_request);
+    return http_request;
 }
