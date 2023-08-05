f64e804f301fb0835ff5f678a68782b6100530b1
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-08-04 22:23:33 -0400

    Respond to TCP connection attempts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+
+    for (connection_id, stream) in listener.incoming().enumerate() {
+        let _stream = stream?;
+        println!("Connection #{}", connection_id + 1);
+    }
+    
+    return Ok(());
 }
