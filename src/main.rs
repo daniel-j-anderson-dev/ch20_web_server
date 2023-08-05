a295d81f9b80830c8c21c5baa3fa1d87376c11a2
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-08-05 11:04:44 -0400

    Attempt to rewrite http parsing from book ver

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
     error::Error,
@@ -9,33 +10,60 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     for (connection_id, stream) in listener.incoming().enumerate() {
         println!("Handling Connection #{}", connection_id + 1);
-        handle_connection(stream?)?;
+        handle_connection_book_ver(stream?)?;
     }
         
     return Ok(());
 }
 
-fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
-    let http_request: Vec<String> = read_http_request(&mut stream)?;
-    println!("{http_request:#?}");
+fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    let http_request = read_http_request(&mut stream)?;
 
-    let response: &[u8] = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
-    stream.write_all(response)?;
-    
+    let response: String = build_html_response(r"E:\src\rust\web_server\example.html")?;
+        
+    stream.write_all(response.as_bytes())?;
+
     return Ok(());
+}
+
+fn handle_connection_book_ver(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    let http_request: Vec<String> = read_http_request_book_ver(&mut stream);
+
+    let response: String = build_html_response(r"E:\src\rust\web_server\example.html")?;
+    
+    stream.write_all(response.as_bytes())?;
+
+    return Ok(());
+}
+
+fn build_html_response(html_file_path: &str) -> Result<String, std::io::Error> {
+    let status_line: &str = "HTTP/1.1 200 OK";
+    let contents: String = fs::read_to_string(html_file_path)?;
+    let length: usize = contents.len();
+    let response: String = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    return Ok(response);
 }
 
 fn read_http_request(stream: &mut TcpStream) -> Result<Vec<String>, std::io::Error> {
     let buf_reader: BufReader<&TcpStream> = BufReader::new(stream);
-
     let mut http_request: Vec<String> = Vec::new();
-    
-    for line in buf_reader.lines().into_iter() {
+    for (line_id, line) in buf_reader.lines().enumerate() {
+        println!("unwrapping line #{} with ?", line_id+1);
         let line: String = line?;
         if !line.is_empty() {
             http_request.push(line);
         };
+        println!("line #{} collected", line_id+1);
     }
-    
     return Ok(http_request);
+}
+
+fn read_http_request_book_ver(stream: &mut TcpStream) -> Vec<String> {
+    let buffer_reader = BufReader::new(stream);
+    let http_request = buffer_reader
+        .lines()
+        .map(|result| result.expect("io error"))
+        .take_while(|line| !line.is_empty())
+        .collect();
+    return http_request;
 }
