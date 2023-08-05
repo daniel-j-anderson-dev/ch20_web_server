f193bebe9e1184a46b44c1ba99c822aa6bbd6482
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-08-05 11:17:14 -0400

    Removed prints

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ use std::{
 fn main() -> Result<(), Box<dyn Error>> {
     let listener: TcpListener = TcpListener::bind("127.0.0.1:7878")?;
 
-    for (connection_id, stream) in listener.incoming().enumerate() {
-        println!("Handling Connection #{}", connection_id + 1);
+    for stream in listener.incoming() {
         handle_connection_book_ver(stream?)?;
     }
         
@@ -39,25 +38,24 @@ fn handle_connection_book_ver(mut stream: TcpStream) -> Result<(), std::io::Erro
 fn build_html_response(html_file_path: &str) -> Result<String, std::io::Error> {
     let status_line: &str = "HTTP/1.1 200 OK";
     let contents: String = fs::read_to_string(html_file_path)?;
-    let length: usize = contents.len();
-    let response: String = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response: String = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status_line, contents.len(), contents);
     return Ok(response);
 }
 
+// why doesn't this work
 fn read_http_request(stream: &mut TcpStream) -> Result<Vec<String>, std::io::Error> {
     let buf_reader: BufReader<&TcpStream> = BufReader::new(stream);
     let mut http_request: Vec<String> = Vec::new();
-    for (line_id, line) in buf_reader.lines().enumerate() {
-        println!("unwrapping line #{} with ?", line_id+1);
+    for line in buf_reader.lines() {
         let line: String = line?;
         if !line.is_empty() {
             http_request.push(line);
         };
-        println!("line #{} collected", line_id+1);
     }
     return Ok(http_request);
 }
 
+// why does this work
 fn read_http_request_book_ver(stream: &mut TcpStream) -> Vec<String> {
     let buffer_reader = BufReader::new(stream);
     let http_request = buffer_reader
