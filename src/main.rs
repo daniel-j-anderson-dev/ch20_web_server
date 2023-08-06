6de1ddf9a9bcaa7fe3cb16ef5f3b95361d3649cb
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-08-06 13:02:08 -0400

    Changed request from Vec<String> to String

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{self, prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    time::Duration,
+    thread,
     error::Error,
 };
 
@@ -9,15 +11,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let listener: TcpListener = TcpListener::bind("127.0.0.1:7878")?;
 
     for (connection_id, stream) in listener.incoming().enumerate() {
-        let (request, response)= handle_connection(stream?)?;
-        println!("CONNECTION {}\nREQUEST\n{:#?}\nRESPONSE\n{}", connection_id + 1, request, response);
+        let (request, response) = handle_connection(stream?)?;
+        println!("CONNECTION {}\nREQUEST\n{}\nRESPONSE\n{}\n", connection_id + 1, request, response);
     }
         
     return Ok(());
 }
 
-fn handle_connection(mut stream: TcpStream) -> Result<(Vec<String>, String), io::Error> {
-    let request: Vec<String> = read_request(&mut stream)?;
+fn handle_connection(mut stream: TcpStream) -> Result<(String, String), io::Error> {
+    let request: String = read_request(&mut stream)?;
     let response: String = parse_request(&request)?;
     
     stream.write_all(response.as_bytes())?;
@@ -25,29 +27,30 @@ fn handle_connection(mut stream: TcpStream) -> Result<(Vec<String>, String), io:
     return Ok((request, response));
 }
 
-fn read_request(stream: &mut TcpStream) -> Result<Vec<String>, io::Error> {
+fn read_request(stream: &mut TcpStream) -> Result<String, io::Error> {
     let buf_reader: BufReader<&mut TcpStream>  = BufReader::new(stream);
-    let mut http_request: Vec<String> = Vec::new();
+    let mut http_request: String = String::new();
     for line in buf_reader.lines() {
         let line: String = line?;
         if line.is_empty() { break };
-        http_request.push(line);
+        http_request.push_str(&line);
+        http_request.push('\n');
     }
     return Ok(http_request);
 }
 
-fn parse_request(request: &Vec<String>) -> Result<String, io::Error> {
-    let request_line = match request.get(0) {
-        Some(first_line) => first_line,
-        None => ""
-    };
+fn parse_request(request: &str) -> Result<String, io::Error> {
+    let request_line = request.lines().next().unwrap();
     
-    let (status, content_path) = if request_line == "Get / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", r"E:\src\rust\web_server\html\example.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", r"E:\src\rust\web_server\html\404.html")
+    let (status, content_path) = match &request_line[..] {
+        "GET / HTTP/1.1"      => ("HTTP/1.1 200 OK", r"html\hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", r"html\hello.html")
+        },
+        "GET /exit HTTP/1.1"  => std::process::exit(0),
+        _                     => ("HTTP/1.1 404 NOT FOUND", r"html\404.html"),
     };
-
     return build_response(status, content_path);
 }
 
