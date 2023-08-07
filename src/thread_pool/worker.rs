67c95e116df3495500caa60071324b96adb8b090
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-08-07 01:19:46 -0400

    Worker::new now gets a job from the receiver.

--- src/thread_pool/worker.rs
+++ src/thread_pool/worker.rs
@@ -20,10 +20,27 @@ pub struct Worker {
     thread: thread::JoinHandle<()>
 }
 impl Worker {
+    /// Executes the closure on an avliable thread, or it goes in the queue
+    /// 
+    /// The closure must return a Result<T, E>
+    /// 
+    /// where
+    /// 
+    ///     T: ()
+    /// 
+    ///     E: crate::thread_pool:error::Error
+    /// 
+    /// trait object ex: Box<dyn std::error::Error>
     pub fn new(id: usize, receiver: Receiver) -> Result<Worker, Error> {
-        let thread: JoinHandle<Arc<Mutex<mpsc::Receiver<Job>>>> = thread::Builder::new()
-            .spawn(|| {
-                receiver    
+        let thread: JoinHandle<()> = thread::Builder::new()
+            .spawn(move || {
+                loop {
+                    let job = receiver.lock().unwrap().recv().unwrap();
+                    
+                    println!("Worker {id} got a job; executing\n");
+
+                    job();
+                }
             })
             .map_err(|error| Error::StdIo(error))?;
         return Ok(Worker { id, thread, });
