6c5e43209d420d028b05e17c3f939bfdae860571
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-08-07 06:55:30 -0400

    Updated documentation.

--- src/thread_pool/worker.rs
+++ src/thread_pool/worker.rs
@@ -24,20 +24,16 @@ impl Worker {
     ///     T: ()
     /// 
     ///     E: crate::thread_pool:error::Error
-    /// 
-    /// trait object ex: Box<dyn std::error::Error>
     pub fn new(id: usize, receiver: Receiver) -> Result<Worker, Error> {
         let thread: JoinHandle<()> = thread::Builder::new()
             .spawn(move || {
                 loop {
                     let job: Job = receiver
-                        
                         .lock()
                             .unwrap_or_else(|error| {
                                 println!("{error}");
                                 std::process::exit(2);
                             })
-
                         .recv()
                             .unwrap_or_else(|error| {
                                 println!("{error}");
