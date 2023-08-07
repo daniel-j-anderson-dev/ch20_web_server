6aea2dbec618804fdaae06f30648c5987fbc9b70
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-08-06 21:15:37 -0400

    Added workers to ThreadPool

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::thread::{self, Thread};
+
 #[derive(Debug)]
 pub enum PoolCreationError {
     PoolSizeZero,
@@ -19,18 +21,22 @@ impl std::error::Error for PoolCreationError {
 
 type ThreadExecutionError = Box<dyn std::error::Error>;
 
-pub struct ThreadPool;
+pub struct ThreadPool {
+    workers: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
     /// 
     /// The pool_size is the number of threads in the returned pool.
     pub fn new(pool_size: usize) -> Result<ThreadPool, PoolCreationError> {
-        if pool_size > 0 {
-            return  Ok(ThreadPool)
-        } else {
+        if pool_size == 0 {
             return Err(PoolCreationError::PoolSizeZero);
         }
+
+        let workers: Vec<thread::JoinHandle<()>>  = Vec::with_capacity(pool_size);
+        
+        return  Ok(ThreadPool { workers, })
     }
     pub fn execute<F>(&self, f: F,) -> Result<(String, String), ThreadExecutionError>
     where
